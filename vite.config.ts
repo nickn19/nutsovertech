fe30f2dcc53ea4c004b286a535048adecc637f76
nick <nick@nutsovertech.com>
2025-07-29 18:59:43 +0530

    static copu

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,11 @@
 import react from "@vitejs/plugin-react";
 import tailwind from "tailwindcss";
 import { defineConfig } from "vite";
-import { viteStaticCopy } from "vite-plugin-static-copy"; // 🔥 Add this
 
 export default defineConfig({
   plugins: [
     react(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: "public/_redirects", // copy this file
-          dest: ".",                // to dist/
-        },
-      ],
-    }),
+   
   ],
   base: "/",
   css: {
